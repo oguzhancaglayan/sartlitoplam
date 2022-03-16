7cd1519a0ed4674288d12debb90537e80bdbec4a
Oğuzhan Çağlayan <86193977+oguzhancaglayan@users.noreply.github.com>
2022-03-16 23:56:50 +0300

    Add files via upload

---
+++ pratiksayitoplam.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class pratiksayitoplam {
+    public static void main(String[] args) {
+          int n;
+          int total = 0;
+          Scanner scan = new Scanner(System.in);
+
+       do {
+
+            System.out.print("Sayı giriniz:  ");
+            n= scan.nextInt();
+
+             if(n % 4 == 0){
+                total += n;
+
+
+             }
+       } while ( n % 2 == 0);
+
+       System.out.println("Toplam : " +total);
+    }
+}
